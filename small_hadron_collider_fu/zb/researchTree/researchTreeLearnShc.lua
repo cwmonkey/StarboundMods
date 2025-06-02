82bceac5b0d57397500fcbdaf8061354988cfcc6
cwmonkey <wackoburns@hotmail.com>
2025-06-02 11:42:22 -0700

    fix comment

--- small_hadron_collider_fu/zb/researchTree/researchTreeLearnShc.lua
+++ small_hadron_collider_fu/zb/researchTree/researchTreeLearnShc.lua
@@ -3,7 +3,7 @@ small_hadron_collider_fuInit = init
 function init()
 	small_hadron_collider_fuInit()
 
-	-- if basic crops is researched, give the spring flower recipe
+	-- if "fission" is researched, give the shc recipe
 	if isResearched("fu_power", "FISSION") then
 		if not player.blueprintKnown("shc") then
 			player.giveBlueprint("shc")
